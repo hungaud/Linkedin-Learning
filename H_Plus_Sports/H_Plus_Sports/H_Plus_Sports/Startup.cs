69036183406d6bb5e125b73ae1c5dad091bb7b2d
hungaud <hung.auduong@hotmail.com>
2018-12-02 21:29:02 -0800

    checked in controllers' skeleton

--- H_Plus_Sports/H_Plus_Sports/H_Plus_Sports/Startup.cs
+++ H_Plus_Sports/H_Plus_Sports/H_Plus_Sports/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using H_Plus_Sports.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,7 +27,11 @@ namespace H_Plus_Sports
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
+
+            //var connection = "Data Source=DESKTOP-2NNMV13;Initial Catalog=H_Plus_Sports;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<H_Plus_SportsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("H_Plus_Sports_Database")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
